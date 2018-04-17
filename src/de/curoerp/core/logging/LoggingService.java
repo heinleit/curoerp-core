adcf325cf42bb60d02dbc624d6571dc22ef053aa
Hendrik Heinle <info@hendrik-heinle.de>
2018-04-17 21:37:20 +0200

    logging

--- src/de/curoerp/core/logging/LoggingService.java
+++ src/de/curoerp/core/logging/LoggingService.java
@@ -1,22 +1,17 @@
 package de.curoerp.core.logging;
 
 
-import java.io.File;
-import java.net.MalformedURLException;
-
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 
 public class LoggingService {
 
 	private Logger logger;
 
 	public LoggingService() {
-		try {
-			System.setProperty("log4j.configurationFile", new File("resources", "log4j.xml").toURI().toURL().toString());
-		} catch (MalformedURLException e) {
-			LoggingService.error(e);
-		}
+		Configurator.setLevel("CuroLogger", Level.INFO);
 		this.logger = LogManager.getLogger("CuroLogger");//"curoerp-core");
 	}
 
