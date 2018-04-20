6cb6dbb50050a70852dcdd7aac14a83ef8493cd6
Hendrik Heinle <hendrik.heinle@jtl-software.com>
2018-04-20 19:04:11 +0200

    #3 parse dependency and limitation

---
+++ src/de/curoerp/core/modularity/dependency/DependencyInfo.java
@@ -0,0 +1,6 @@
+package de.curoerp.core.modularity.dependency;
+
+public class DependencyInfo {
+	public String name;
+	public DependencyLimitation[] limitations;
+}
--- src/de/curoerp/core/modularity/module/Module.java
+++ src/de/curoerp/core/modularity/module/Module.java
@@ -3,18 +3,28 @@ package de.curoerp.core.modularity.module;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Array;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.jar.JarFile;
+
+import javax.naming.LimitExceededException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
+import de.curoerp.core.modularity.dependency.DependencyInfo;
+import de.curoerp.core.modularity.dependency.DependencyLimitation;
+import de.curoerp.core.modularity.exception.DependencyLimitationException;
 import de.curoerp.core.modularity.exception.ModuleCanNotBeLoadedException;
 import de.curoerp.core.modularity.exception.ModuleFileAlreadyLoadedException;
 import de.curoerp.core.modularity.exception.ModuleVersionStringInvalidException;
+import de.curoerp.core.modularity.versioning.VersionExpression;
 import de.curoerp.core.modularity.versioning.VersionInfo;
+import de.curoerp.core.modularity.versioning.VersionService;
 
 /**
  * Module-Model for..
@@ -24,12 +34,13 @@ import de.curoerp.core.modularity.versioning.VersionInfo;
  * @since 15.04.2018
  */
 public class Module implements IModule {
-	
+
 	private File file;
 	private boolean isLoaded = false;
 	private ModuleInfo info;
 	private VersionInfo version;
-	
+	private DependencyInfo[] dependencies;
+
 	/**
 	 * Construct Module by Jar-File
 	 * 
@@ -38,9 +49,9 @@ public class Module implements IModule {
 	public Module(File file) {
 		this.file = file;
 	}
-	
-	
-	
+
+
+
 	/*
 	 * #########################################################
 	 * #                                                       #
@@ -48,7 +59,7 @@ public class Module implements IModule {
 	 * #                                                       #
 	 * #########################################################
 	 */
-	
+
 	/**
 	 * Get System-Name
 	 * 
@@ -58,7 +69,7 @@ public class Module implements IModule {
 		if(!this.isLoaded) return null;
 		return Module.parseSystemName(this.info.name);
 	}
-	
+
 	/**
 	 * parse module name for system
 	 * 
@@ -68,7 +79,7 @@ public class Module implements IModule {
 	public static String parseSystemName(String name) {
 		return name.toLowerCase().trim().replace(' ', '_');
 	}
-	
+
 	/**
 	 * Get Display Name (Name and Version)
 	 * 
@@ -78,7 +89,7 @@ public class Module implements IModule {
 		if(!this.isLoaded) return null;
 		return this.info.name + " (" + this.version + ")";
 	}
-	
+
 	/**
 	 * Get Module-Version
 	 * 
@@ -88,7 +99,7 @@ public class Module implements IModule {
 		if(!this.isLoaded) return null;
 		return this.version;
 	}
-	
+
 	/**
 	 * Get Dependencies
 	 * 
@@ -98,7 +109,7 @@ public class Module implements IModule {
 		if(!this.isLoaded) return null;
 		return this.info.dependencies;
 	}
-	
+
 	/**
 	 * Get Dependency-Resolve Types
 	 * 
@@ -108,7 +119,7 @@ public class Module implements IModule {
 		if(!this.isLoaded) return null;
 		return this.info.typeInfos;
 	}
-	
+
 	/**
 	 * Get Boot-Class for booting after heaping and dependency resolvement
 	 * 
@@ -119,8 +130,8 @@ public class Module implements IModule {
 		return this.info.bootClass;
 	}
 
-	
-	
+
+
 	/*
 	 * #########################################################
 	 * #                                                       #
@@ -128,7 +139,7 @@ public class Module implements IModule {
 	 * #                                                       #
 	 * #########################################################
 	 */
-	
+
 	/**
 	 * Fetch module-information from jar-file/cmod.yml
 	 * 
@@ -140,21 +151,78 @@ public class Module implements IModule {
 			JarFile jarFile = new JarFile(this.file);
 			InputStream stream = jarFile.getInputStream(jarFile.getEntry("cmod.yml"));
 			ModuleInfo info = mapper.readValue(stream, ModuleInfo.class);
-			
+
 			//cleanup
 			stream.close();
 			jarFile.close();
-			
+
 			// VersionInfo
 			this.version = new VersionInfo(info.version);
-			
+
 			// ModuleInfo
 			this.info = info;
-		} catch (IOException | ModuleVersionStringInvalidException e) {
+
+			// Dependencies
+			this.parseDependencies();
+		} catch (IOException | ModuleVersionStringInvalidException | DependencyLimitationException e) {
 			throw new ModuleCanNotBeLoadedException(e.getMessage());
 		}
 	}
-	
+
+	/**
+	 * TODO: Doku
+	 * @throws DependencyLimitationException
+	 */
+	private void parseDependencies() throws DependencyLimitationException {
+		ArrayList<DependencyInfo> dependencies = new ArrayList<>();
+
+		for (String sDependency : this.info.dependencies) {
+			DependencyInfo dependency = new DependencyInfo();
+			String[] splittedDependency = sDependency.split(":", 2);
+
+			if(splittedDependency.length == 2) {
+				// there are limitations
+				ArrayList<DependencyLimitation> limitations = new ArrayList<>();
+				for (String sLimitation : splittedDependency[1].split(",")) {
+					DependencyLimitation limitation = Arrays.stream(VersionExpression.values())
+							.filter(expression -> sLimitation.startsWith(expression.pattern))
+							.map(expression -> parseLimitation(sLimitation.substring(0, expression.pattern.length()), expression))
+							.filter(l -> l != null)
+							.findFirst()
+							.orElse(null);
+					
+					if(limitation == null) {
+						throw new DependencyLimitationException("dependency-limitation '" + sLimitation + "' could not resolved!");
+					}
+					limitations.add(limitation);
+				}
+				dependency.limitations = limitations.toArray(new DependencyLimitation[limitations.size()]);
+			}
+
+			dependency.name = splittedDependency[0];
+			dependencies.add(dependency);
+		}
+
+		this.dependencies = dependencies.toArray(new DependencyInfo[dependencies.size()]);
+	}
+
+	/**
+	 * TODO: Doku
+	 * @param sVersion
+	 * @param expression
+	 * @return
+	 */
+	private DependencyLimitation parseLimitation(String sVersion, VersionExpression expression) {
+		DependencyLimitation limitation = new DependencyLimitation();
+		limitation.expression = expression;
+		try {
+			limitation.version = new VersionInfo(sVersion);
+		} catch (ModuleVersionStringInvalidException e) {
+			return null;
+		}
+		return limitation;
+	}
+
 	/**
 	 * heap jar-file in actual runtime
 	 * 
@@ -165,7 +233,7 @@ public class Module implements IModule {
 		if(this.isLoaded) {
 			throw new ModuleFileAlreadyLoadedException();
 		}
-		
+
 		Method method = null;
 		try {
 			URLClassLoader classLoader = (URLClassLoader)ClassLoader.getSystemClassLoader();
@@ -181,8 +249,8 @@ public class Module implements IModule {
 				method.setAccessible(false);
 			}
 		}
-		
+
 		this.isLoaded = true;
 	}
-	
+
 }
