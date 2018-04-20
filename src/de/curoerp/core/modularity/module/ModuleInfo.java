f16d1cd1da1b509fe15a12bb7039fae199a95dd9
Hendrik Heinle <hendrik.heinle@jtl-software.com>
2018-04-20 17:30:08 +0200

    #5 Helper class ModuleInfo and subclasses outward nesting
    Ignore: TypeInfo

--- src/de/curoerp/core/modularity/module/ModuleInfo.java
+++ src/de/curoerp/core/modularity/module/ModuleInfo.java
@@ -2,30 +2,10 @@ package de.curoerp.core.modularity.module;
 
 public class ModuleInfo {
 	
-	private String name;
-	private int version;
-	private String[] dependencies = new String[0];
-	private TypeInfo[] typeInfos = new TypeInfo[0];
-	private String bootClass;
-	
-	public String getName() {
-		return name;
-	}
-	
-	public int getVersion() {
-		return version;
-	}
-	
-	public String[] getDependencies() {
-		return dependencies;
-	}
-	
-	public TypeInfo[] getTypeInfos() {
-		return typeInfos;
-	}
-	
-	public String getBootClass() {
-		return bootClass;
-	}
+	public String name;
+	public int version;
+	public String[] dependencies = new String[0];
+	public TypeInfo[] typeInfos = new TypeInfo[0];
+	public String bootClass;
 	
 }
